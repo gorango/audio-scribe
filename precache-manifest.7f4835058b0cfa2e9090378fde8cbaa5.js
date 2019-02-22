f71fd3442eb8ceb51d3893191e0e00ebc6a67009
Goran <gospaso@gmail.com>
2019-02-22 15:56:11 -0500

    02ee9a0

---
+++ precache-manifest.7f4835058b0cfa2e9090378fde8cbaa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "75749f9d3b9de3593823",
+    "url": "/js/chunk-vendors.0499cc01.js"
+  },
+  {
+    "revision": "7f643f14b5c70bc05765",
+    "url": "/js/app.6fbe1645.js"
+  },
+  {
+    "revision": "5314586bc62758efc8b432c57eaee6bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f643f14b5c70bc05765",
+    "url": "/css/app.781555bb.css"
+  }
+];
